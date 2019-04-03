29d729312259106c0cfad52a0dcf0f2c9fe1cb0a
Christopher Stock <christopher.stock@mayflower.de>
2019-04-03 10:53:23 +0200

    Added Xcode's initial sceleton app.

---
+++ DevCamp2019_SwiftSpriteKitWorkshop/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DevCamp2019_SwiftSpriteKitWorkshop
+//
+//  Created by Christopher Stock on 03.04.19.
+//  Copyright © 2019 Mayflower GmbH. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ DevCamp2019_SwiftSpriteKitWorkshop/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DevCamp2019_SwiftSpriteKitWorkshop
+//
+//  Created by Christopher Stock on 03.04.19.
+//  Copyright © 2019 Mayflower GmbH. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
